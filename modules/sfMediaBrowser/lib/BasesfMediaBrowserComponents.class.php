ced1d83e5fc480e7630d01ac73108eb9b6066851
alex-pex <alexandre.paixao@stadline.com>
2015-06-17 11:30:58 +0200

    Beautify PHP

--- modules/sfMediaBrowser/lib/BasesfMediaBrowserComponents.class.php
+++ modules/sfMediaBrowser/lib/BasesfMediaBrowserComponents.class.php
@@ -1,20 +1,16 @@
 <?php
 
 /**
- *
- *
  * @package     sfMediaBrowser
  * @author      Vincent Agnano <vincent.agnano@particul.es>
  */
 class BasesfMediaBrowserComponents extends sfComponents
 {
-  public function executeIcon(sfWebRequest $request)
-  {
-    $class = sfMediaBrowserUtils::getTypeFromExtension(sfMediaBrowserUtils::getExtensionFromFile($this->file_url)) == 'image'
-           ? 'sfMediaBrowserImageObject'
-           : 'sfMediaBrowserFileObject'
-           ;
-    $this->file = new $class($this->file_url);
-  }
-  
+    public function executeIcon(sfWebRequest $request)
+    {
+        $type = sfMediaBrowserUtils::getTypeFromExtension(sfMediaBrowserUtils::getExtensionFromFile($this->file_url));
+        $class = ($type == 'image') ? 'sfMediaBrowserImageObject' : 'sfMediaBrowserFileObject';
+
+        $this->file = new $class($this->file_url);
+    }
 }
